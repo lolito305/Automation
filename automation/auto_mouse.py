b023e51145ddaec1b55cd9c836163cebcd583c27
lolito305 <74833887+lolito305@users.noreply.github.com>
2023-06-14 18:14:19 -0700

    Add files via upload

---
+++ automation/auto_mouse.py
@@ -0,0 +1,9 @@
+import pyautogui as pag
+import time
+import random
+
+while True:
+    x = random.randint(600, 700)
+    y = random.randint(200, 600)
+    pag.moveTo(x, y, duration=0.5)
+    time.sleep(1)
